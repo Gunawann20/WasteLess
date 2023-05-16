7deefc94b95d7b03fa0727225724576d4908d3cb
Galang Ramadhan <galang.rambu.ramadhan.99@gmail.com>
2023-05-17 00:49:53 +0700

    Upload module

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+//middleware untuk parsing body dari request
+app.use
+
+// Menjalankan server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
